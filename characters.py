1b579d44a31aa2a3fe23e68885b8e1633da7d23e
Dave Canache <davehcanache@gmail.com>
2024-07-08 02:55:54 -0400

    If its attacking then it won't move

--- characters.py
+++ characters.py
@@ -1,18 +1,16 @@
 import pygame
 
-from bot import Bot
-
-bot= Bot(500,360)
 class Pj():
     def __init__(self, x,y):
         self.rect = pygame.Rect((x,y,80,200))
         self.jump_vel = 0
         self.jump=False
-        self.orientation = ["left"]
+        self.orientation = "left"
         self.rect = pygame.Rect((x,y,80,200))
         self.health = 310
+        self.attacking = False
 
-    def move(self, screen_width, screen_height,screen,target,):
+    def move(self, screen_width, screen_height,screen,target):
         SPEED = 15
         GRAVITY = 2
         dx = 0
@@ -21,33 +19,36 @@ class Pj():
         #Key Pressed
         key = pygame.key.get_pressed()
 
-        #Movement
-        if key[pygame.K_a] and key[pygame.K_j]==False  and key[pygame.K_k]==False:
-            dx-=SPEED
+        #If not attacking the player can move
+        if self.attacking == False:
+            #Movement
+            if key[pygame.K_a] and key[pygame.K_j]==False  and key[pygame.K_k]==False:
+                dx-=SPEED
 
-        if key[pygame.K_d] and key[pygame.K_j]==False  and key[pygame.K_k]==False:
-            dx+=SPEED
+            if key[pygame.K_d] and key[pygame.K_j]==False  and key[pygame.K_k]==False:
+                dx+=SPEED
 
-        #Jump
-        if key[pygame.K_w] and self.jump==False:
-            self.jump_vel = -30
-            self.jump=True
-        
-        self.jump_vel += GRAVITY
-        dy += self.jump_vel
-            
-        #Attacks
-        if key[pygame.K_j]:
-            
-            if self.orientation[0]=="left":
-                self.attack_punch(screen, (200,0,0), target)
-            else:    
-                self.attack_punch(screen, (200,0,0), target)
+            #Jump
+            if key[pygame.K_w] and self.jump==False:
+                self.jump_vel = -30
+                self.jump=True
+
+            #Attacks
+            if key[pygame.K_j]:
+                if self.orientation=="left":
+                    self.attack_punch(screen, (200,0,0), target)
+                else:    
+                    self.attack_punch(screen, (200,0,0), target)
     
-        if key[pygame.K_k]:
+            if key[pygame.K_k]:
+                self.attack_lowkick(screen,(200,0,0),target)
+        #If not attacking
+        self.attacking = False
 
-            self.attack_lowkick(screen,(200,0,0),target)
-            
+        #Gravity
+        self.jump_vel += GRAVITY
+        dy += self.jump_vel 
+
         #Margin
         if self.rect.left + dx < 0:
             dx = -self.rect.left
@@ -60,38 +61,34 @@ class Pj():
             self.jump = False
             dy = screen_height - 40 - self.rect.bottom
   
-        if self.rect[0]	> bot.rect[0]:
-            
-            del self.orientation[0]
-            self.orientation.append("right")
-            
+        if self.rect[0]	> target.rect[0]:
+            self.orientation = "right"
         else:
-            del self.orientation[0]
-            self.orientation.append("left")
+            self.orientation = "left"
 
         #Update Player
         self.rect.x += dx
         self.rect.y += dy
        
     def attack_lowkick(self,screen, color, target):
-
-            orientation = self.orientation[0]
+            orientation = self.orientation
             if orientation == "left":
                 atacking_rect= pygame.draw.rect(screen,color,(self.rect.x+80,self.rect.y+160,40,40))
             else:
                 atacking_rect= pygame.draw.rect(screen,color,(self.rect.x-40,self.rect.y+160,40,40))
             if atacking_rect.colliderect(target.rect) and target.health > 0:
                 target.health -= 15
+            self.attacking = True
                 
-    def attack_punch(self,screen,color,target):
-            
-            orientation = self.orientation[0]
+    def attack_punch(self,screen,color,target):    
+            orientation = self.orientation
             if orientation == "left":
                 atacking_rect= pygame.draw.rect(screen,color,(self.rect.x+80,self.rect.y+40,40,40)) 
             else:
                 atacking_rect= pygame.draw.rect(screen,color,(self.rect.x-40,self.rect.y+40,40,40))   
             if atacking_rect.colliderect(target.rect) and target.health > 0:
                 target.health -= 5
+            self.attacking = True
                 
     def draw(self, surf):
         pygame.draw.rect(surf, (0,0,200),self.rect)
